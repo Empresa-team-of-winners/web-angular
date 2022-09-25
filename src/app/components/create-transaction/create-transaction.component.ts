fff7b83e10e3d964e504808ce567efd816090c4d
Macxxon <maicolalarcon93@gmail.com>
2022-09-24 22:00:54 -0500

    dev: create transaction

--- src/app/components/create-transaction/create-transaction.component.ts
+++ src/app/components/create-transaction/create-transaction.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../services/users/api.service';
+import { formatDate } from '@angular/common';
+import { Router } from '@angular/router';
+
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-create-transaction',
@@ -7,9 +18,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTransactionComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+  companyForm: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    private router: Router,
+    public apiService: ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.companyForm = this.fb.group({
+      name: ['', Validators.required],
+      document: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: ['', Validators.required],
+      users: ['', Validators.required],
+      createdAt: [formatDate(new Date(), 'dd-MM-yyyy', 'en'), Validators.required],
+      updatedAt: [formatDate(new Date(), 'dd-MM-yyyy', 'en'), Validators.required],
+    });
+
+    this.getAllUsers();
+  }
+
+  guardar(): void {
+    this.apiService.saveEnterprise(this.companyForm.value).subscribe(resp => {
+      this.companyForm.reset();
+      this.companyForm.setErrors(null);
+      this.router.navigate(['company']);
+    },
+      error => { console.error(error) }
+    )
+  }
+
+  
+  getAllUsers():void{
+    this.apiService.getUsers().subscribe(resp => {
+      console.log(resp);
+      this.users = resp;
+    },
+      error => { console.error(error) }
+    )
   }
 
 }
